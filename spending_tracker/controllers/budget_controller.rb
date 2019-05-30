58963a462b8484e99ad00e74984ab325f0195240
Adamsh6 <adam.s.houten@gmail.com>
2019-05-30 22:32:36 +0100

    budget index shows current budget, merch/tag index shows merch/tags

--- spending_tracker/controllers/budget_controller.rb
+++ spending_tracker/controllers/budget_controller.rb
@@ -5,5 +5,6 @@ require_relative('../models/budget')
 also_reload( '../models/*' )
 
 get '/budget' do
+  @latest_budget = Budget.all.last
   erb ( :"budgets/index" )
 end
--- spending_tracker/controllers/merchant_and_tag_controller.rb
+++ spending_tracker/controllers/merchant_and_tag_controller.rb
@@ -6,5 +6,7 @@ require_relative('../models/tag')
 also_reload( '../models/*' )
 
 get '/merchants_and_tags' do
+  @merchants = Merchant.all
+  @tags = Tag.all
   erb(:"merchants_and_tags/index")
 end
