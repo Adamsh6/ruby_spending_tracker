d8656260c332809203548dfd4cdd2ab650d8841b
Adamsh6 <adam.s.houten@gmail.com>
2019-05-31 18:43:57 +0100

    added ability to change budget on budget viewer page

--- spending_tracker/controllers/budget_controller.rb
+++ spending_tracker/controllers/budget_controller.rb
@@ -10,3 +10,10 @@ get '/budget' do
   # @total_spent = Transaction.total_transaction_amount
   erb ( :"budgets/index" )
 end
+
+post '/budget' do
+  @latest_budget = Budget.last_budget
+  @latest_budget.amount = params['amount']
+  @latest_budget.update
+  redirect('/budget')
+end
