e4063f9bf3dad109d108d5886c0e4902d7721a00
Adamsh6 <adam.s.houten@gmail.com>
2019-06-01 11:00:35 +0100

    now displays budgets table on app

--- spending_tracker/controllers/budget_controller.rb
+++ spending_tracker/controllers/budget_controller.rb
@@ -3,11 +3,16 @@ require('sinatra/contrib/all')
 
 require_relative('../models/budget')
 require_relative('../models/transaction')
+require_relative('../models/date_handler')
 also_reload( '../models/*' )
 
 get '/budget' do
   @latest_budget = Budget.last_budget
   @total_spent = Transaction.total_transaction_amount
+
+  @budgets = Budget.last_six_months
+  @total_transactions = Transaction.last_six_months_amounts
+  @months = DateHandler.last_six_months
   erb ( :"budgets/index" )
 end
 
